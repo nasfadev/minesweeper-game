79f8fa53223a8984a887c5933130171dbcf30ced
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-23 15:51:24 +0700

    fix chekered tiles grid style when theconfig size is even number and change the grid color style

--- js/grid-generator.js
+++ js/grid-generator.js
@@ -6,13 +6,12 @@ const gridWidth = mainCanvas.width / Config.size;
 export function renderAll() {
   let chekeredFlag = false;
   for (let i = 0; i < Config.size; i++) {
+    chekeredFlag = chekeredFlag ? false : true;
     for (let j = 0; j < Config.size; j++) {
       ctx.beginPath();
-      ctx.fillStyle = chekeredFlag ? "blue" : "red";
+      ctx.fillStyle = chekeredFlag ? "#24A7C3" : "#11D4FF";
       ctx.fillRect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
       ctx.rect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
-      ctx.fillStyle = "black";
-      ctx.stroke();
       chekeredFlag = chekeredFlag ? false : true;
     }
   }
