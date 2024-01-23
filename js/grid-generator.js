bc7cf367ffa2e22d41fd1a8ebf0011373a816e4e
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-23 16:49:50 +0700

    added autoChekeredGrid feature

--- js/grid-generator.js
+++ js/grid-generator.js
@@ -5,13 +5,26 @@ const ctx = Canvas.ctx;
 export function renderAll() {
   let checkeredFlag = false;
   for (let i = 0; i < Config.size; i++) {
-    checkeredFlag = checkeredFlag ? false : true;
     for (let j = 0; j < Config.size; j++) {
+      let index = Config.size * i + j;
+      checkeredFlag = autoCheckeredGrid(checkeredFlag, Config.size, index);
       ctx.beginPath();
       ctx.fillStyle = checkeredFlag ? "#24A7C3" : "#11D4FF";
       ctx.fillRect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
       ctx.rect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
       checkeredFlag = checkeredFlag ? false : true;
     }
+  }
+}
+
+function autoCheckeredGrid(currentCheckeredFlag, size, index) {
+  if (size % 2 == 0) {
+    if (Math.floor(index / size) % 2 == 0) {
+      return (currentCheckeredFlag = index % 2 == 0);
+    } else {
+      return (currentCheckeredFlag = index % 2 != 0);
+    }
+  } else {
+    return (currentCheckeredFlag = index % 2 != 0);
   }
 }
