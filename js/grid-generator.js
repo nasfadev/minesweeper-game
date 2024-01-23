0f1e46a22ee756c869cff756ff0577d2890f77d1
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-23 16:04:14 +0700

    fix typo checkeredFlag

--- js/grid-generator.js
+++ js/grid-generator.js
@@ -3,15 +3,15 @@ import * as Canvas from "./canvas.js";
 const gridWidth = Canvas.main.width / Config.size;
 const ctx = Canvas.ctx;
 export function renderAll() {
-  let chekeredFlag = false;
+  let checkeredFlag = false;
   for (let i = 0; i < Config.size; i++) {
-    chekeredFlag = chekeredFlag ? false : true;
+    checkeredFlag = checkeredFlag ? false : true;
     for (let j = 0; j < Config.size; j++) {
       ctx.beginPath();
-      ctx.fillStyle = chekeredFlag ? "#24A7C3" : "#11D4FF";
+      ctx.fillStyle = checkeredFlag ? "#24A7C3" : "#11D4FF";
       ctx.fillRect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
       ctx.rect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
-      chekeredFlag = chekeredFlag ? false : true;
+      checkeredFlag = checkeredFlag ? false : true;
     }
   }
 }
