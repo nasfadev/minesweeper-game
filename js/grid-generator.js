e1077ad8f74b56971234dde2cdc02c50d1a4b4c0
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-23 15:45:46 +0700

    added chekered tiles feature

--- js/grid-generator.js
+++ js/grid-generator.js
@@ -4,15 +4,16 @@ const mainCanvas = document.getElementById("main-canvas");
 const ctx = mainCanvas.getContext("2d", { alpha: false });
 const gridWidth = mainCanvas.width / Config.size;
 export function renderAll() {
+  let chekeredFlag = false;
   for (let i = 0; i < Config.size; i++) {
     for (let j = 0; j < Config.size; j++) {
       ctx.beginPath();
-      ctx.fillStyle = "blue";
+      ctx.fillStyle = chekeredFlag ? "blue" : "red";
       ctx.fillRect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
       ctx.rect(gridWidth * j, gridWidth * i, gridWidth, gridWidth);
       ctx.fillStyle = "black";
       ctx.stroke();
+      chekeredFlag = chekeredFlag ? false : true;
     }
-    console.log(i);
   }
 }
