329c96f7b7b761f80ccab5cc6fcbf03417667518
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-24 10:01:48 +0700

    fix canvas responsive bug when in mobile screen, added partialRender function in grid-generator.js and link this function to event-handler.js

--- js/event-handler.js
+++ js/event-handler.js
@@ -1,6 +1,7 @@
 import { main as canvas } from "./canvas.js";
+import { partialRender } from "./grid-generator.js";
 export function init() {
   canvas.addEventListener("click", (e) => {
-    console.log(e);
+    partialRender(e);
   });
 }
--- js/grid-generator.js
+++ js/grid-generator.js
@@ -15,6 +15,20 @@ export function renderAll() {
     }
   }
 }
+export function partialRender(event) {
+  let y = Math.floor(event.offsetX / (event.target.offsetHeight / Config.size));
+  let x = Math.floor(event.offsetY / (event.target.offsetHeight / Config.size));
+  let index = Config.size * y + x;
+  let checkeredFlag;
+  checkeredFlag = autoCheckeredGrid(checkeredFlag, Config.size, index);
+  ctx.beginPath();
+  ctx.fillStyle = checkeredFlag ? "#D2D2D2" : "#E1E0E0";
+  ctx.fillRect(gridWidth * y, gridWidth * x, gridWidth, gridWidth);
+  ctx.rect(gridWidth * y, gridWidth * x, gridWidth, gridWidth);
+  // console.log(x + "-" + y + " grid" + gridWidth);
+  // console.log(Canvas.main.width / Config.size);
+  // console.log(event);
+}
 
 function autoCheckeredGrid(currentCheckeredFlag, size, index) {
   if (size % 2 == 0) {
