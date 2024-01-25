99631644a8efd123b2af1aa16e2481e54500e664
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-25 16:44:02 +0700

    added some property in Theme Class to support flag icon feature

--- js/config.js
+++ js/config.js
@@ -8,4 +8,6 @@ export class Theme {
   static defaultFontSize = 1000;
   static boomIconUrl = "./img/bomb-icon.png";
   static boomIconSize = 0.9;
+  static flagIconUrl = "./img/flag-icon.png";
+  static flagIconSize = 0.9;
 }
