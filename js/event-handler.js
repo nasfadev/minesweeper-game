456d29c5c3814ba488d7f1030d45ecd6d45a4419
Pauza Nasrullah <fauziannasrullah@gmail.com>
2024-01-25 15:55:21 +0700

    added new event handler for cintext menu in event-handler.js

--- js/event-handler.js
+++ js/event-handler.js
@@ -2,9 +2,15 @@ import { main as canvas } from "./canvas.js";
 import { partialRender } from "./grid-generator.js";
 // init event
 export function init() {
+  // register event listener for prevent context menu
+  canvas.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
   // register event listener for canvas element
-  canvas.addEventListener("click", (e) => {
+  canvas.addEventListener("mouseup", (e) => {
     // render just one part of the grid
+    console.log(e);
     partialRender(e);
+    e.preventDefault();
   });
 }
